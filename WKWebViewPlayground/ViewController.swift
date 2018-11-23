9e20b5284e07f23dc96fe34e26dd2525c72841c9
springuper <springuper@gmail.com>
2018-11-23 16:52:09 +0800

    add local html page

--- WKWebViewPlayground/ViewController.swift
+++ WKWebViewPlayground/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController, WKNavigationDelegate {
+class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
     @IBOutlet weak var webview: WKWebView!
 
     override func loadView() {
@@ -17,6 +17,7 @@ class ViewController: UIViewController, WKNavigationDelegate {
         configuration.allowsInlineMediaPlayback = false
         configuration.mediaTypesRequiringUserActionForPlayback = []
         webview = WKWebView(frame: .zero, configuration: configuration)
+        webview.uiDelegate = self
         webview.navigationDelegate = self
         view = webview
     }
@@ -24,10 +25,21 @@ class ViewController: UIViewController, WKNavigationDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let url = URL(string: "https://web-1.staging-public.tubi.io/webview/content/428736/everybody_hates_keisha")!
+        // let url = URL(string: "https://web-1.staging-public.tubi.io/webview/content/428736/everybody_hates_keisha")!
         // let url = URL(string: "https://google.com")!
-        webview.load(URLRequest(url: url))
-        webview.allowsBackForwardNavigationGestures = true
+        // webview.load(URLRequest(url: url))
+        // webview.allowsBackForwardNavigationGestures = true
+
+
+        // load test.html
+        let path:String = Bundle.main.path(forResource: "test", ofType: "html")!
+        let textContent = try? String(contentsOfFile: path, encoding: String.Encoding.utf8)
+        webview.loadHTMLString(textContent!, baseURL: nil);
+    }
+
+    func webView(_ webView: WKWebView, runJavaScriptTextInputPanelWithPrompt prompt: String, defaultText: String?, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (String?) -> Void) {
+        print("prompt in ios", prompt);
+        completionHandler("pong");
     }
 }
 
